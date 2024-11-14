430d3d0a8452b5352f713b1dacd8d9777703fbc5
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2024-11-14 15:26:09 -0500

    added regex expressions for name, number and email

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,11 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"John\sCena|Anya\sTaylor\-Joy|D\'Angelo"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"5{10}|5{3}\-5{3}\-5{4}|\(5{3}\)\s5{3}\-5{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"johncena\@wwe\.com|[A-z]*\@[A-z]{3}\.[A-z]{3}|[A-z]*\.[A-z]*\@[A-z]{3}\.[A-z]{3}|[A-z]*\.[A-z]*[0-9]*\@[A-z]{3}\.[A-z]{3}"
 email_regex = re.compile(email_address)
